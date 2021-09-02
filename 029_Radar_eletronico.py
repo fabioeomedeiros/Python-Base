f8e3af03a3c84ddd884646f8d0737b3331462113
Fabio Medeiros <fabioeomedeiros@gmail.com>
2021-09-01 23:15:24 -0300

    Adição

--- 029_Radar_eletronico.py
+++ 029_Radar_eletronico.py
@@ -1,8 +1,13 @@
 # 029_Radar_eletronico.py
-#
+# Resebe um valor de velocidade e calcula a multa se for apropriado
+# velocidade máx = 80km/h e multa = R$7,00 por km/h ultrapassado
 
 print()
-
+vel = float(input("Velocidade registrada: "))
 print()
-
+if (vel > 80):
+    multa = (vel-80)*7
+    print(f"Você foi multado em R${multa:.2f}")
+else:
+    print("Dirija com cuidado!")
 print()
--- 030_Par_ou_impar.py
+++ 030_Par_ou_impar.py
@@ -1,1 +1,11 @@
 # 030_Par_ou_impar.py
+# verifica de um número digitado é par ou ímpar
+
+print()
+num = int(input("Entre com um número inteiro: "))
+print()
+if (num % 2 == 0):
+    print(f"{num} é PAR")
+else:
+    print(f"{num} é ÍMPAR")
+print()
