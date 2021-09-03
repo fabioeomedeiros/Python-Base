f675410091dbc0c985ffd1e78491317290612730
Fabio Medeiros <fabioeomedeiros@gmail.com>
2021-09-03 16:52:24 -0300

    add

--- 053_Detector_de_palindromo.py
+++ 053_Detector_de_palindromo.py
@@ -1,2 +1,20 @@
 #053_Detector_de_palindromo.py
 
+frase = str(input("Digite uma frase: ")).strip().upper()
+palavras = frase.split(" ")
+junto = "".join(palavras)
+# com fatiamento
+inverso = junto[::-1]
+
+# Sem fatiamento
+# inverso = ""
+# for i in range(len(junto)-1, -1, -1):
+#    inverso = inverso + junto[i]
+
+print(junto)
+print(inverso)
+
+if (junto == inverso):
+    print("É palíndromo")
+else:
+    print("Não é palíndromo")
--- 054_Grupo_da_maioridade.py
+++ 054_Grupo_da_maioridade.py
@@ -1,1 +1,25 @@
 #054_Grupo_da_maioridade.py
+
+from datetime import date
+
+lista_nasc = []
+lista_idade = []
+ano = date.today().year
+menor = 0
+maior = 0
+
+for i in range(1, 6):
+    nasc = int(input(f"Ano de nascimento da {i}ª pessoa: "))
+    idade = ano - nasc
+    lista_nasc.append(nasc)
+    lista_idade.append(idade)
+    if (idade < 18):
+        menor += 1
+    else:
+        maior += 1
+
+for i in range(1, 6):
+    print(f"{i}ª PESSOA | NASC: {lista_nasc[(i-1)]} | IDADE: {lista_idade[(i-1)]}")
+
+print(f"{menor} pessoas são MENORES de idade")
+print(f"{maior} pessoas são MAIORES de idade")
