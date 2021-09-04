92a26e44484836e194342a869d2487d81cc86f7c
Fabio Medeiros <fabioeomedeiros@gmail.com>
2021-09-03 22:43:08 -0300

    add

--- 059_Menu_de_opcoes.py
+++ 059_Menu_de_opcoes.py
@@ -1,1 +1,31 @@
 #059_Menu_de_opcoes.py
+
+n1 = float(input("1º Número: "))
+n2 = float(input("2º Número: "))
+op = 0
+while (op != 5):
+    print("[ 1 ] Somar")
+    print("[ 2 ] Multiplicar")
+    print("[ 3 ] Maior")
+    print("[ 4 ] Novos números")
+    print("[ 5 ] Finalizar")
+    op = int(input("----> "))
+
+    if (op == 1):
+        print(n1 + n2)
+    elif (op == 2):
+        print(n1 * n2)
+    elif (op == 3):
+        if (n1 > n2):
+            print(f"{n1} é o maior")
+        if (n2 > n1):
+            print(f"{n2} é o maior")
+        else:
+            print("Os valores são iguais")
+    elif (op == 4):
+        n1 = float(input("1º Número: "))
+        n2 = float(input("2º Número: "))
+    elif (op == 5):
+        print("Finalizando")
+    else:
+        print("Opção inválida")
--- 060_Calculo_fatorial.py
+++ 060_Calculo_fatorial.py
@@ -1,1 +1,26 @@
 #060_Calculo_fatorial.py
+
+from math import factorial
+
+num = int(input("Entre com o número: "))
+print()
+
+ff = 1
+print(f"{num}! = ", end="")
+for i in range(num, 0 ,-1):
+    print(f"{i}", end="")
+    print(" x " if i > 1 else " = ", end="")
+    ff *= i
+print(ff)
+
+fw = 1
+j = num
+print(f"{j}! = ", end="")
+while j >= 1:
+    print(f"{j}", end="")
+    print(" x " if j > 1 else " = ", end="")
+    fw *= j
+    j -= 1
+print(fw)
+
+print(f"{num}! = {factorial(num)}")
