e14a5dafde3500d0682ec2213b2b294e6befe79f
Fabio Medeiros <fabioeomedeiros@gmail.com>
2021-09-07 01:43:53 -0300

    add

--- 002_Respondendo_o_usuario.py
+++ 002_Respondendo_o_usuario.py
@@ -1,11 +1,24 @@
 # 002_Respondendo_o_usuário.py
 # Recebe dados do usuário, armazena em variáveis e escreve na tela com formatado de formas direfentes
 
+print("")
 nome = str(input("Nome: "))
 idade = int(input("Idade (anos): "))
 altura = float(input("Altura (metros): "))
+print("")
 
+print("1º forma:")
 print(nome, "tem", idade, "anos de idade e", altura, "metros de altura.")
-print("%s tem %d anos de idade e %f metros de altura." % (nome, idade, altura))
-print("{} tem {} anos de idade e {} metros de altura.".format(nome, idade, altura))
-print(f"{nome} tem {idade} anos de idade e {altura} metros de altura.")
+print("")
+
+print("2º forma:")
+print("%s tem %d anos de idade e %.2f metros de altura." % (nome, idade, altura))
+print("")
+
+print("3º forma:")
+print("{} tem {} anos de idade e {:.2f} metros de altura.".format(nome, idade, altura))
+print("")
+
+print("4º forma:")
+print(f"{nome} tem {idade} anos de idade e {altura:.2f} metros de altura.")
+print("")
--- 068_Jogo_do_par_ou_impar.py
+++ 068_Jogo_do_par_ou_impar.py
@@ -6,22 +6,23 @@ v = 0
 while True:
     esc = " "
     c = randint(0,5)
-    while esc not in "pi":
-        esc = str(input("PAR ou ÍMPAR [p/i]? ")).strip().lower()[0]
+    print("")
+    while esc not in "PI":
+        esc = str(input("PAR ou ÍMPAR [P/I]? ")).strip().upper()[0]
 
     j = int(input("Jogue um número (0-5): "))
     print(f"JOGADOR: {j}")
     print(f"COMPUTADOR: {c}")
     print(f"Total: {j + c}")
     print("PAR" if ((j + c) % 2 == 0) else "ÍMPAR")
-    if esc == "p":
+    if esc == "P":
         if ((j + c) % 2 == 0):
             print("Venceu")
             v += 1
         if((j + c) % 2 == 1):
             print("Perdeu")
             break
-    if esc == "i":
+    if esc == "I":
         if((j + c) % 2 == 0):
             print("Perdeu")
             break
